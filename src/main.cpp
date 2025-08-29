b3ce937c1f8ba2625a09a0c39f143b3c904de36c
Ehsan Rashid <ehsan.rashid@gmail.com>
2025-08-29 15:28:50 +0500

    update Logifle name

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 #include "loggerlib.h"
 
 int main(int argc, char** argv) {
-    qLogger::get().initialize("logs/eland_engine.log", LogLevel::DEBUG);
+    qLogger::get().initialize("logs/eLandEngine.log", LogLevel::DEBUG);
 
     // Initialize the factory with default database types
     DatabaseFactory::initialize();
